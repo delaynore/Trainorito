8d7ccf04ce4937f341c100f7805092f2dbab6117
Daniil Lozhkin <delaynore@gmail.com>
2025-03-17 23:54:59 +0400

    add api project

---
+++ src/Trainorito.Api/Controllers/TimeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trainorito.Api.Controllers;
+
+[ApiController]
+public class TimeController : ControllerBase
+{
+
+    [HttpGet("api/current-time")]
+    public IActionResult GetCurrentTimeUtc()
+    {
+        return Ok(DateTime.UtcNow);
+    }
+
+}
